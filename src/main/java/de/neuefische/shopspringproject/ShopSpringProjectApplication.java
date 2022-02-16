642c4b665c61dde524bee0bd896b47c7a6590ab4
Oualid Sahbani <oualidsahbani@me.com>
2022-02-16 13:24:44 +0100

    monProjetFromScratch

---
+++ src/main/java/de/neuefische/shopspringproject/ShopSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.shopspringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopSpringProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopSpringProjectApplication.class, args);
+    }
+
+}
